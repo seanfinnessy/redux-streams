c6bb07c1699e90574cbc7a7a2241fc1e26d1717a
seanfinnessy <seanfinnessy@icloud.com>
2021-04-19 14:55:23 -0400

    Fetch streams reducer added

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -3,6 +3,9 @@ import _ from "lodash";
 export default (state = {}, action) => {
   switch (action.type) {
     // key interpolation syntax. [new key]: value. This way we can add a new key value pair on the fly!
+    case "FETCH_STREAMS":
+      // create an object out of the list that is returned from API. Keys inside obj are the id's.
+      return { ...state, ..._.mapKeys(action.payload, 'id') };
     case "FETCH_STREAM":
       return { ...state, [action.payload.id]: action.payload };
     case "CREATE_STREAM":
