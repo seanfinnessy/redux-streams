3cc46007e6abec5f5ddd08762685a2f927c4ef7a
seanfinnessy <seanfinnessy@icloud.com>
2021-04-22 17:10:48 -0400

    edited stream components

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {fetchStream} from '../../actions';
 
-const StreamEdit = () => {
-  return (
-    <div>
-      StreamEdit
-    </div>
-  )
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    console.log(this.props);
+    if (!this.props.stream) {
+      return <div>Loading...</div>
+    }
+    return (
+      <div>
+        {this.props.stream.title}
+      </div>
+    );
+  };
 }
 
-export default StreamEdit;
+// ownProps to call the props object in our component
+const mapStateToProps = (state, ownProps) => {
+  return {
+    // using Bracket Notation. Used for accessing object properties. object["foo"] = object.foo.
+    stream: state.streams[ownProps.match.params.id] 
+  };
+}
+
+export default connect(mapStateToProps, {fetchStream})(StreamEdit);
