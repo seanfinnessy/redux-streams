db7ef87ab78fd138dee552a7914278f02c56950d
seanfinnessy <seanfinnessy@icloud.com>
2021-05-10 14:32:01 -0400

    full on CRUD complete. All basic crud app.

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -15,7 +15,7 @@ class StreamList extends React.Component {
           {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
-            {stream.title}
+            <Link to={`/streams/${stream.id}`} className="header">{stream.title}</Link>
             <div className="description">{stream.description}</div>
           </div>
         </div>
@@ -28,7 +28,7 @@ class StreamList extends React.Component {
       return (
         <div className="right floated content">
           <Link className="ui button primary" to={`/streams/edit/${stream.id}`}>Edit</Link>
-          <button className="ui button negative">Delete</button>
+          <Link className="ui button negative" to={`/streams/delete/${stream.id}`}>Delete</Link>
         </div>
       )
     }
